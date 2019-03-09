846854cba5bceda7d04883ba6648594eccd544e6
Zann580 <julian580@gmail.com>
2019-03-09 10:13:24 -0600

    Formating.

--- Tic Tac Toe/Tic_Tac_Toe_x2.cpp
+++ Tic Tac Toe/Tic_Tac_Toe_x2.cpp
@@ -19,34 +19,34 @@ int main() {
           cin >> choice;
 
           mark=(player == 1) ? 'X' : 'O' ;
+	  
+	if (choice == 1 && square[1] == '1')
 
-    if (choice == 1 && square[1] == '1')
+		square[1] = mark;
+	else if (choice == 2 && square[2] == '2')
 
-          square[1] = mark;
-    else if (choice == 2 && square[2] == '2')
+		square[2] = mark;
+	else if (choice == 3 && square[3] == '3')
+	
+		square[3] = mark;
+	else if (choice == 4 && square[4] == '4')
+		
+		square[4] = mark;
+	else if (choice == 5 && square[5] == '5')
 
-          square[2] = mark;
-    else if (choice == 3 && square[3] == '3')
+		square[5] = mark;
+	else if (choice == 6 && square[6] == '6')
+		
+		square[6] = mark;
+	else if (choice == 7 && square[7] == '7')
 
-          square[3] = mark;
-    else if (choice == 4 && square[4] == '4')
+		square[7] = mark;
+	else if (choice == 8 && square[8] == '8')
 
-          square[4] = mark;
-    else if (choice == 5 && square[5] == '5')
+		square[8] = mark;
+	else if (choice == 9 && square[9] == '9')
 
-          square[5] = mark;
-		else if (choice == 6 && square[6] == '6')
-
-			     square[6] = mark;
-		else if (choice == 7 && square[7] == '7')
-
-			     square[7] = mark;
-		else if (choice == 8 && square[8] == '8')
-
-			     square[8] = mark;
-		else if (choice == 9 && square[9] == '9')
-
-			     square[9] = mark;
+		square[9] = mark;
     else
     {
         std::cout<<"Invalid move ";
